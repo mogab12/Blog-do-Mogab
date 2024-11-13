e2f222cee097c826af6d8dfdbdc3343154fe793d
Gabriel Caram <gabrielccaram@usp.br>
2024-11-13 15:20:27 -0300

    Implementação do design do blog

---
+++ README.md
@@ -0,0 +1,23 @@
+# Blog Django
+
+## Descrição
+Projeto de blog desenvolvido como parte de um trabalho acadêmico, utilizando Django.
+
+## Requisitos
+- Python 3.8+
+- Django 3.2+
+
+## Configuração do Ambiente
+
+### Clonar Repositório
+```bash
+git clone <url-do-repositorio>
+cd blog_django
+
+```
+
+### Instalar dependências
+Instale os requerimentos do projeto, listados no arquivo 'requirements.txt', utilize:
+```bash
+    pip install -r requirements.txt
+```
--- blog_project/settings.py
+++ blog_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -118,7 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
